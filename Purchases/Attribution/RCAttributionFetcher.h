85c68d523fed13ce103fe7dbfa4d43636e6f849c
vegaro <cesarvegaro@gmail.com>
2021-07-02 12:17:05 -0700

    fix RCAttributionFetcher.h

--- Purchases/Attribution/RCAttributionFetcher.h
+++ Purchases/Attribution/RCAttributionFetcher.h
@@ -8,8 +8,8 @@
 
 #import <Foundation/Foundation.h>
 #import <StoreKit/StoreKit.h>
-#import "RCAttributionNetwork.h"
 #import "RCAttributionTypeFactory.h"
+@import PurchasesCoreSwift;
 
 NS_ASSUME_NONNULL_BEGIN
 
